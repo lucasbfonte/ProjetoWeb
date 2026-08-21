12f518bc324c64a00ecdd3ec6cd83710c52cb436
Lucas Becker Fonte <lucasfonte@Lucass-MacBook-Neo.local>
2026-08-21 15:26:03 -0300

    Melhores botoes de remover, botoes de concluir e inclusao disso nas materias

--- script.js
+++ script.js
@@ -1,19 +1,73 @@
-let input = document.getElementById("taskInput");
-let addButton = document.getElementById("addButton");
-let removeButton = document.getElementById("removeButton");
+let taskInput = document.getElementById("taskInput");
+let addTaskButton = document.getElementById("addTaskButton");
 let taskList = document.getElementById("taskList");
+let subjectInput = document.getElementById("subjectInput");
+let addSubjectButton = document.getElementById("addSubjectButton");
+let subjectList = document.getElementById("subjectList");
 
+addSubjectButton.addEventListener("click", function() {
+    let subjectAdd = document.createElement("li");
+    subjectAdd.textContent = subjectInput.value;
+    subjectList.appendChild(subjectAdd);
+    subjectInput.value = "";
 
-addButton.addEventListener("click", function() {
+    let removeSubjectButton = document.createElement("button");
+    removeSubjectButton.textContent = " Remover";
+    subjectAdd.appendChild(removeSubjectButton);
+    removeSubjectButton.addEventListener("click", function() {
+        subjectList.removeChild(subjectAdd);
+    });
+});
+
+addTaskButton.addEventListener("click", function() {
     let taskAdd = document.createElement("li");
-    taskAdd.textContent = input.value;
+    taskAdd.textContent = taskInput.value;
     taskList.appendChild(taskAdd);
-    input.value = "";   
+    taskInput.value = "";  
+
+    let completeButton = document.createElement("button");
+    completeButton.textContent = "";
+    taskAdd.appendChild(completeButton);
+    let isComp = false;
+    completeButton.addEventListener("click", function() {
+        if (!isComp){
+            completeButton.textContent = "✓";
+            completeButton.style.color = "green";
+            taskAdd.style.textDecoration = "line-through";
+            isComp = true;
+        } else {
+            completeButton.textContent = "";
+            completeButton.style.color = "black";
+            taskAdd.style.textDecoration = "none";
+            isComp = false;
+        }
+    });
+    completeButton.addEventListener("mouseover", function() {
+        if (!isComp){
+            completeButton.textContent = "✓";
+            completeButton.style.color = "green";
+        } else {
+            completeButton.textContent = "✗"; 
+            completeButton.style.color = "red";
+        }
+    });
+    completeButton.addEventListener("mouseout", function() { 
+        if (!isComp){
+            completeButton.textContent = "";
+        } else {
+            completeButton.textContent = "✓";
+            completeButton.style.color = "green";
+        }
+    });
+
+    
+    let removeTaskButton = document.createElement("button");
+    removeTaskButton.textContent = " Remover";
+    taskAdd.appendChild(removeTaskButton);
+    removeTaskButton.addEventListener("click", function() {
+        taskList.removeChild(taskAdd);
+    });
 });
 
-removeButton.addEventListener("click", function() {
-    let taskRemove = taskList.lastElementChild;
-    if (taskRemove) {
-        taskList.removeChild(taskRemove);
-    }
-});
+    
+
